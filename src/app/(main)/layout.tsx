e581eba62d1e97e23dca9587ec8e876af4cdd055
flan02 <chanivetdan@hotmail.com>
2025-11-20 14:14:33 -0300

    Checkout. Testing Vercel deployment

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -22,7 +22,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
       {
         session.user.name &&
         <>
-          {/* <AuthTabSync /> */}
+
           <ClientTabGuard />
           <Marquee time={30} banners={marquee_banners} />
           <Navbar />
